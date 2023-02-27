dbcc89502ac00c7668b5818b22147df3ac413b67
HazelDaniel <hazeldaniel856@gmail.com>
2023-02-27 04:01:00 -0600

    working on positional parameters

---
+++ params.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "My name is `basename $0` - i was called as $0"
+echo "I was called with $# parameters."
+if [ "$#" -eq "2" ]; then
+  echo "first parameter was : $1"
+  echo "second parameter was : $2"
+else
+  echo "Usage: `basename $0` [first_parameter] [second_parameter]"
+  echo "2 parameters required, but you provided $#"
+fi
