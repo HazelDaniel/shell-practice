37be7651a86bf952c1aa30ac19a16367be3bc46b
HazelDaniel <hazeldaniel856@gmail.com>
2023-03-04 05:50:18 -0600

    added parameter recognition to the hacker_cypher shell script

--- hacker_cypher.sh
+++ hacker_cypher.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-trap cleanup 1 2
+trap cleanup 1 2 5
 
 cleanup(){
  echo "i was running \"$BASH_COMMAND\" when you halted me. currently at line \"$LINENO\" in the script"
   exit 1
 }
-while read hackLine; do clear; echo -n $hackLine; sleep .1; done < hacker_cypher.txt;
+if [ $1 ];
+then
+  while read hackLine; do clear; echo -n $hackLine; sleep .1; done < $1;
+
+  else 
+    echo "invalid parameter this program requires one valid parameter"
+
+fi
