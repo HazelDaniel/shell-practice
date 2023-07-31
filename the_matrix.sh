49e9fc898fcc5613a77c01c43b10b10ba80a706f
HazelDaniel <hazeldaniel856@gmail.com>
2023-07-31 11:54:25 +0100

    added more dynamism to the art

--- the_matrix.sh
+++ the_matrix.sh
@@ -5,11 +5,11 @@ while : ;
 do
 	if [[ -n $LINES && -n $COLUMNS ]];
 	then
-		echo $LINES $COLUMNS $(( "$RANDOM" % "$COLUMNS" + 1)) $(printf "\U$(( $RANDOM % 500))");
+		echo $LINES $COLUMNS $(( ("$RANDOM" % "$COLUMNS") + 1)) "$(printf "\U$(( ($RANDOM % 500) + 1))")";
 		sleep 0.05;
 	fi
-	((LINES += 2))
-	((COLUMNS += 2))
+	((LINES += (RANDOM % 5)))
+	((COLUMNS += (RANDOM % 3)))
 done | gawk '{a[$3]=0; for (x in a){o=a[x]; a[x]=a[x]+1; printf "\033[%s;%sH\033[2;32m%s",o,x,$4;\
 	printf "\033[%s;%sH\033[1;37m%s\033[0;0H",a[x],x, $4;\
 	if (a[x]>=$1){a[x]=0;}}}'
