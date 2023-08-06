75ead968c4557b59eb932ad74464d88f16377258
HazelDaniel <hazeldaniel856@gmail.com>
2023-08-06 14:15:49 +0100

    updated the cypher text

--- hacker_cypher.sh
+++ hacker_cypher.sh
@@ -6,7 +6,7 @@ cleanup(){
  echo "i was running \"$BASH_COMMAND\" when you halted me. currently at line \"$LINENO\" in the script"
   exit 1
 }
-if [ $1 ];
+if [[ -n $1 ]];
 then
   while read hackLine; do clear; echo -n $hackLine; sleep .1; done < $1;
 
